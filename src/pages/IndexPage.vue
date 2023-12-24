23200cc66362a01a3eaad0b3ddb76f2f3171117d
mohammadjavad.ebrahimi <mohammadjavad.ebrahimi@partdp.ir>
2023-12-24 17:24:18 +0330

    feat(): make login page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="flex column items-center login q-pa-xl">
+    <div class="flex items-start full-width">
+      <q-icon name="arrow_back" />
+    </div>
+    <q-img class="login__image" src="/src/assets/Group_3552.svg" />
+    <div class="flex column full-width login__form">
+      <q-input mask="(####) ### - ####" filled bg-color="white" class="full-width " placeholder="(0915) 235 - 9821"
+        dense="true" />
+
+      <q-btn class="full-width login__form-btn" label="ورود صاحب كار" />
+    </div>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+<script setup>
 </script>
+<style lang="scss" scoped>
+.login {
+  gap: 2rem;
+
+  &__form {
+    gap: 1rem;
+
+    &-btn {
+      background-color: #da0037;
+      color: white;
+    }
+  }
+
+  &__image {
+    max-height: 477px;
+    max-height: 477px;
+  }
+}
+</style>
+
